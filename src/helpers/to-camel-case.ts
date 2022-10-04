66ba5403d5dfc4681f9ecf69d8c1339d6c071b0f
Snaer Halldorsson <mysnowytime@gmail.com>
2022-10-04 14:14:54 -0600

    Added the camel case helper

--- src/helpers/to-camel-case.ts
+++ src/helpers/to-camel-case.ts
@@ -1,1 +1,24 @@
-export const toCamelCase = (what: string) => {};
+/**
+ * pretty ugly but converts all the following into camelCase
+ * -- snake_case -> camelCase
+ * -- kebab-case -> camelCase
+ * -- PascalCase -> camelCase
+ */
+export const toCamelCase = (str: string) => {
+    if (typeof str !== "string") return str;
+    if (/(?:_)|(?:-)/.test(str)) {
+        // we this word is most likely snake_case
+        return str
+            .toLowerCase()
+            .replace(/([-_][a-z])/g, (group) =>
+                group.toUpperCase().replace("-", "").replace("_", "")
+            );
+    }
+    return str
+        .replace(/(?:^\w|[A-Z]|[_-]|\b\w)/g, (word, index) => {
+            return index === 0 ? word.toLowerCase() : word.toUpperCase();
+        })
+        .replace(/\s+/g, "");
+};
+
+console.log(toCamelCase("kebab-case"));
